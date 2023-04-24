5c5d7055cb6d6cb575498f50b81130def816e401
Yunus Çağlıyan <yunuscagliyan97@gmail.com>
2023-04-24 12:29:48 +0300

    Added images.

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -2,7 +2,7 @@ object AppConfig {
     const val compileSdk = 33
     const val minSdk = 21
     const val targetSdk = 33
-    const val versionCode = 21
+    const val versionCode = 22
     const val versionName = "3.0.0"
     const val buildToolsVersion = "29.0.3"
     const val applicationId = "com.yunuscagliyan.cinemaapp"
