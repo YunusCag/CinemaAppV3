cd1019a1003ffb345831178305aa117fafda0346
Yunus Çağlıyan <yunuscagliyan97@gmail.com>
2023-07-28 17:28:14 +0300

    3.0.1 Released.

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -2,8 +2,8 @@ object AppConfig {
     const val compileSdk = 34
     const val minSdk = 21
     const val targetSdk = 34
-    const val versionCode = 22
-    const val versionName = "3.0.0"
+    const val versionCode = 23
+    const val versionName = "3.0.1"
     const val buildToolsVersion = "29.0.3"
     const val applicationId = "com.yunuscagliyan.cinemaapp"
     const val jvmTarget = "1.8"
